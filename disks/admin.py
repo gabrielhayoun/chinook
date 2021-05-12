a3be8adcd20f3418f83dfd36a6cc76c6cb44df03
Tanguy Pérennou <Tanguy.Perennou@isae-supaero.fr>
2021-05-12 13:23:03 +0200

    Custom admin for Albums

--- disks/admin.py
+++ disks/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Track, Album, Artist
 
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist')
+    list_filter = ('artist', )
+    search_fields = ('title', )
+
+
 # Register your models here.
 admin.site.register(Track)
-admin.site.register(Album)
+admin.site.register(Album, AlbumAdmin)
 admin.site.register(Artist)
